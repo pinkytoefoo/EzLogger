b8bbf28b8a7b32fddb351335514b4aee4d74c1a0
pinkytoefoo <62123015+pinkytoefoo@users.noreply.github.com>
2025-12-29 18:18:26 -0500

    started `detail` namespace and ansi support on windows supported terminals

--- include/ezlog/ezlog.h
+++ include/ezlog/ezlog.h
@@ -22,13 +22,29 @@
 #   include <unistd.h>
 #endif
 
-// #include <iostream>
 #include <string>
 #include <format>
 
 #ifdef EZ_WINDOWS
-#   define SET_COLOR(color) SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ::ezlog::toWinAttr(color))
+static const HANDLE g_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+#define SET_COLOR(color) SetConsoleTextAttribute(g_handle, ::ezlog::win_attr(color))
 #endif
+
+namespace detail
+{
+    void quick_print(std::string_view s)
+    {
+        std::fputs(s.data(), stdout);
+        std::fputc('\n', stdout);
+    }
+
+    template<typename... Args>
+    void quick_print(std::format_string<Args...> str, Args&&... args)
+    {
+        std::fputs(std::format(str, std::forward<Args>(args)...).c_str(), stdout);
+        std::fputc('\n', stdout);
+    }
+}
 
 namespace ezlog
 {
@@ -52,22 +68,103 @@ namespace ezlog
         bright_white,
     };
 
-    constexpr std::string_view toAnsi(color c) {
-        switch (c) {
-            case color::red:        return "\e[0;31m";
-            case color::green:      return "\e[0;32m";
-            case color::yellow:     return "\e[0;33m";
-            case color::blue:       return "\e[0;34m";
-            case color::magenta:    return "\e[0;35m";
-            case color::cyan:       return "\e[0;36m";
-            case color::white:      return "\e[0;37m";
-            case color::default_:   return "\e[0;39m";
-            default:                return "\e[0;00m";
+    // TODO: move these two functions into a seperate namespace
+    // user does not need access to these helper functions
+    constexpr std::string_view ansi(color c);
+    constexpr unsigned short win_attr(color c);
+
+    enum class level
+    {
+        trace, info, warn, error,
+    };
+
+    class ezlog
+    {
+    public:
+        ezlog()
+            : level_{level::trace}
+        {
+            char* wt_session = getenv("WT_SESSION");
+            if(wt_session != nullptr && strnlen(wt_session, 64) > 0) // 64 should be enough bytes
+            {
+                #define EZ_POSIX
+            }
+        }
+        ezlog(level lvl)
+        : level_{lvl}
+        {
+        }
+        ~ezlog() = default;
+
+        void log(const std::string& msg, color c = color::default_)
+        {
+            write(msg, c);
+        }
+        
+        void trace(const std::string& msg)
+        {
+            log_if(msg, level::trace);
+        }
+
+        void info(const std::string& msg)
+        {
+            log_if(msg, level::info, color::green);
+        }
+
+        void warn(const std::string& msg)
+        {
+            log_if(msg, level::warn, color::yellow);
+        }
+
+        void error(const std::string& msg)
+        {
+            log_if(msg, level::error, color::red);
+        }
+    
+    private:
+        void write(std::string_view msg, color c)
+        {
+            #ifdef EZ_POSIX
+            detail::quick_print("{}{}{}", ansi(c), msg, ansi(color::default_));
+            #elifdef EZ_WINDOWS
+            SET_COLOR(c);
+            detail::quick_print("{}", msg);
+            SET_COLOR(color::default_);
+            #endif
+        }
+
+        void log_if(const std::string& msg, level lvl, color c = color::default_)
+        {
+            if(lvl >= level_)
+                write(msg, c);
+        }
+
+        level level_{level::trace};
+    };
+
+    // TODO: this is a copout to avoid windows not being able to find ansi definition
+    // make this cleaner (line 124)
+    #ifdef EZ_POSIX
+    constexpr std::string_view ansi(color c)
+    {
+        switch (c)
+        {
+            case color::red:        return "\033[31m";
+            case color::green:      return "\033[32m";
+            case color::yellow:     return "\033[33m";
+            case color::blue:       return "\033[34m";
+            case color::magenta:    return "\033[35m";
+            case color::cyan:       return "\033[36m";
+            case color::white:      return "\033[37m";
+            // case color::default_:   return "\033[38m";
+            default:                return "\033[00m";
         }
     }
-
-    constexpr WORD toWinAttr(color c) {
-        switch (c) {
+    #elifdef EZ_WINDOWS 
+    constexpr unsigned short win_attr(color c)
+    {
+        switch (c)
+        {
             case color::black:          return 0x0000;
             case color::blue:           return 0x0001;
             case color::green:          return 0x0002;
@@ -80,68 +177,5 @@ namespace ezlog
             default:                    return 0x0007;
         }
     }
-
-    enum class level
-    {
-        trace, info, warn, error,
-    };
-
-    class ezlog
-    {
-    public:
-        ezlog() = default;
-        ~ezlog() = default;
-        ezlog(level lvl)
-            : lvl_{lvl}
-        {
-        }
-
-        void log(const std::string& msg, color c = color::default_)
-        {
-            #ifdef EZ_POSIX
-            std::println("{}{}{}", toAnsi(c), msg, toAnsi(color::default_));
-            #elifdef EZ_WINDOWS
-            SET_COLOR(c);
-            std::println("{}", msg);
-            SET_COLOR(color::default_);
-            #endif
-        }
-        
-        void trace(const std::string& msg)
-        {
-            _logIf(msg, level::trace);
-        }
-
-        void info(const std::string& msg)
-        {
-            _logIf(msg, level::info, color::green);
-        }
-
-        void warn(const std::string& msg)
-        {
-            _logIf(msg, level::warn, color::yellow);
-        }
-
-        void error(const std::string& msg)
-        {
-            _logIf(msg, level::error, color::red);
-        }
-    
-    private:
-        void _logIf(const std::string& msg, level lvl, color c = color::default_)
-        {
-            if(lvl >= lvl_)
-            {
-                #ifdef EZ_POSIX
-                std::println("{}{}{}", toAnsi(c), msg, toAnsi(color::default_));
-                #elifdef EZ_WINDOWS
-                SET_COLOR(c);
-                std::println("{}", msg);
-                SET_COLOR(color::default_);
-                #endif
-            }
-        }
-
-        level lvl_;
-    };
+    #endif
 }
